1f3f721d54bab06b3a31eb52d5104b2c1602ccf5
wantox86 <waonex86@gmail.com>
2026-04-29 17:44:18 +0700

    Update models.ts

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -21,6 +21,47 @@ export namespace analyzer {
 
 export namespace storage {
 	
+	export class Project {
+	    ID: number;
+	    Name: string;
+	    Path: string;
+	    DetectedStack: string[];
+	    AppliedTemplates: number[];
+	    // Go type: time
+	    CreatedAt: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new Project(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.ID = source["ID"];
+	        this.Name = source["Name"];
+	        this.Path = source["Path"];
+	        this.DetectedStack = source["DetectedStack"];
+	        this.AppliedTemplates = source["AppliedTemplates"];
+	        this.CreatedAt = this.convertValues(source["CreatedAt"], null);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
 	export class Rule {
 	    ID: number;
 	    TemplateID: number;
@@ -43,29 +84,6 @@ export namespace storage {
 	        this.Content = source["Content"];
 	        this.Priority = source["Priority"];
 	        this.Enabled = source["Enabled"];
-	    }
-	}
-	export class Project {
-	    ID: number;
-	    Name: string;
-	    Path: string;
-	    DetectedStack: string[];
-	    AppliedTemplates: number[];
-	    // Go type: time
-	    CreatedAt: any;
-
-	    static createFrom(source: any = {}) {
-	        return new Project(source);
-	    }
-
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.ID = source["ID"];
-	        this.Name = source["Name"];
-	        this.Path = source["Path"];
-	        this.DetectedStack = source["DetectedStack"];
-	        this.AppliedTemplates = source["AppliedTemplates"];
-	        this.CreatedAt = source["CreatedAt"];
 	    }
 	}
 	export class Template {
